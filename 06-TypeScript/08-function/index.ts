ae6c37cc8f52c7b94179b7d0fe4ed703ef84a853
bablubhai <bublufromrizvi@gmail.com>
2025-04-11 22:41:49 +0530

    adding code of optional and default parameters

--- 06-TypeScript/08-function/index.ts
+++ 06-TypeScript/08-function/index.ts
@@ -7,3 +7,24 @@ greet = (name) => {
 };
 
 greet("Arif"); // Hello Arif
+
+//Optional and Default Parameters
+
+function greetUser(name: string, age?: number) {
+    console.log("Hi", name);
+    if (age) {
+      console.log("You are", age, "years old.");
+    }
+  }
+  
+  greetUser("Arif");
+  greetUser("Ali", 25);
+  
+  // ✅ Default
+  function sayHello(name: string = "Guest") {
+    console.log("Hello", name);
+  }
+  
+  sayHello(); // Hello Guest
+  sayHello("Arif"); // Hello Arif
+  
